0b6ae589a69b2bec648c5964e9613f23e79e0ed5
okristenicole9 <okristenicole9@yahoo.com>
2021-02-15 21:30:04 -0600

    wednesday

--- db/seed.js
+++ db/seed.js
@@ -1,31 +1,22 @@
 const {
     client,
     getAllUsers,
-    createUser
+    createUser,
+    updateUser,
   } = require('./index');
-
-  async function createInitialUsers() {
-    try {
-      console.log("Starting to create users...");
-      const albert = await createUser({ username: 'albert', password: 'bertie99' });
-      const sandra = await createUser({ id: 2, username: 'sandra', password: '2sandy4me' });
-      const glamgal = await createUser({ id: 3, username: 'glamgal', password: 'soglam' });
-      console.log(albert);
-  
-      console.log("Finished creating users!");
-    } catch(error) {
-      console.error("Error creating users!");
-      throw error;
-    }
-  }
   
   async function dropTables() {
     try {
       console.log("Starting to drop tables...");
   
       await client.query(`
+      DROP TABLE IF EXISTS posts;
+    `);
+
+      await client.query(`
         DROP TABLE IF EXISTS users;
       `);
+
   
       console.log("Finished dropping tables!");
     } catch (error) {
@@ -34,15 +25,20 @@ const {
     }
   }
   
+
+
   async function createTables() {
     try {
       console.log("Starting to build tables...");
   
       await client.query(`
         CREATE TABLE users (
-          id SERIAL PRIMARY KEY,
-          username varchar(255) UNIQUE NOT NULL,
-          password varchar(255) NOT NULL
+            id SERIAL PRIMARY KEY,
+            username varchar(255) UNIQUE NOT NULL,
+            password varchar(255) NOT NULL,
+            name VARCHAR(255) NOT NULL,
+            location VARCHAR(255) NOT NULL,
+            active BOOLEAN DEFAULT true
         );
       `);
   
@@ -53,32 +49,75 @@ const {
     }
   }
   
-  async function rebuildDB() {
+  async function createInitialUsers() {
     try {
-        client.connect();
-    
-        await dropTables();
-        await createTables();
-        await createInitialUsers();
-      } catch (error) {
-        throw error;
-      }
-  }
+      console.log("Starting to create users...");
   
-  async function testDB() {
-    try {
-      console.log("Starting to test database...");
+      await createUser({ username: 'albert', password: 'bertie99', name:'albert',location:'Louisiana', active: true });
+      await createUser({ username: 'sandra', password: '2sandy4me', name:'sandra',location:'Louisiana', active: true });
+      await createUser({ username: 'glamgal', password: 'soglam', name:'soglam',location:'Louisiana', active: false });
   
-      const users = await getAllUsers();
-      console.log("getAllUsers:", users);
-  
-      console.log("Finished database tests!");
+      console.log("Finished creating users!");
     } catch (error) {
-      console.error("Error testing database!");
+      console.error("Error creating users!");
       throw error;
     }
   }
   
+  async function rebuildDB() {
+    try {
+      client.connect();
+  
+      await dropTables();
+      await createTables();
+      await createInitialUsers();
+    } catch (error) {
+      throw error;
+    }
+  }
+  
+  async function testDB() {
+    try {
+        console.log("Starting to test database...");
+    
+        console.log("Calling getAllUsers")
+        const users = await getAllUsers();
+        console.log("Result:", users);
+    
+        console.log("Calling updateUser on users[0]")
+        const updateUserResult = await updateUser(users[0].id, {
+          name: "Newname Sogood",
+          location: "Lesterville, KY"
+        });
+        console.log("Result:", updateUserResult);
+    
+        console.log("Finished database tests!");
+      } catch (error) {
+        console.error("Error testing database!");
+        throw error;
+      }
+  }
+
+  async function createPosts() {
+    try {
+      console.log("Starting to build posts...");
+  
+      await client.query(`
+        CREATE TABLE posts (
+            id SERIAL PRIMARY KEY,
+            "authorId" INTEGER REFERENCES users(id) NOT NULL,
+            title VARCHAR(255) NOT NULL,
+            content TEXT NOT NULL,
+            active BOOLEAN DEFAULT true
+        );
+      `);
+  
+      console.log("Finished building tables!");
+    } catch (error) {
+      console.error("Error building tables!");
+      throw error;
+    }
+  }
   
   rebuildDB()
     .then(testDB)
